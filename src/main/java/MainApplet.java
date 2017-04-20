42dc99a1fb6fca03ff29128b92a255c08b94a1a4
sanjay900 <sanjay.govind9@gmail.com>
2017-04-20 15:20:42 +1200

    Initial commit

---
+++ src/main/java/MainApplet.java
@@ -0,0 +1,23 @@
+import net.tangentmc.processing.ProcessingRunner;
+import processing.core.PApplet;
+
+public class MainApplet extends PApplet{
+    public static void main(String[] args) {
+        ProcessingRunner.run(new MainApplet());
+    }
+    public void settings() {
+
+    }
+    public void setup() {
+
+    }
+    public void draw() {
+
+    }
+    public void keyPressed() {
+
+    }
+    public void keyReleased() {
+
+    }
+}
